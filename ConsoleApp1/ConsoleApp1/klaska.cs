818b91dd5b706e8bd0cb4878949c1cd566e0dbec
michalinok <93190@student.san.edu.pl>
2025-01-04 21:54:53 +0100

    Sec commit

---
+++ ConsoleApp1/ConsoleApp1/klaska.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class klaska
+    {
+        int first = 1;
+        int second = 2;
+        int third = 3; 
+        int fourth = 4;
+        int fifth = 5;
+        int sixth = 6;
+        int seventh = 7;
+
+    }
+}
